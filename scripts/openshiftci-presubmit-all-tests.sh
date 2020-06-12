63913d652fef0f31c2a61b4b77e5a6c447ec9f0b
zhengxiaomei123 <xmzheng@cn.ibm.com>
2020-06-12 11:49:53 +0800

    Configure CI scripts to make it work on s390x arch.

--- scripts/openshiftci-presubmit-all-tests.sh
+++ scripts/openshiftci-presubmit-all-tests.sh
@@ -13,15 +13,19 @@ go get -u github.com/onsi/ginkgo/ginkgo
 export PATH="$PATH:$(pwd):$GOPATH/bin"
 export ARTIFACTS_DIR="/tmp/artifacts"
 export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
+export ODO_BOOTSTRAPPER_IMAGE=quay.io/zhengxiaomei123/odo4z:1.1.3-s390
 
 # Integration tests
-make test-integration
-make test-integration-devfile
+make test-generic
+make test-cmd-link-unlink
+make test-cmd-pref-config
+make test-cmd-watch
+make test-cmd-debug
 make test-cmd-login-logout
 make test-cmd-project
 make test-operator-hub
 
 # E2e tests
-make test-e2e-all
+make test-e2e-beta
 
 odo logout
