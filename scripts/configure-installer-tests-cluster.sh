5a34c6f83131a7b7328a3de0a619c7c0092821f8
zhengxiaomei123 <xmzheng@cn.ibm.com>
2020-06-12 11:49:53 +0800

    Configure CI scripts to make it work on s390x arch.

--- scripts/configure-installer-tests-cluster.sh
+++ scripts/configure-installer-tests-cluster.sh
@@ -5,14 +5,14 @@ set -x
 HTPASSWD_FILE="./htpass"
 USERPASS="developer"
 HTPASSWD_SECRET="htpasswd-secret"
-SETUP_OPERATORS="./scripts/setup-operators.sh"
+#SETUP_OPERATORS="./scripts/setup-operators.sh"
 # Overrideable information
 DEFAULT_INSTALLER_ASSETS_DIR=${DEFAULT_INSTALLER_ASSETS_DIR:-$(pwd)}
 KUBEADMIN_USER=${KUBEADMIN_USER:-"kubeadmin"}
 KUBEADMIN_PASSWORD_FILE=${KUBEADMIN_PASSWORD_FILE:-"${DEFAULT_INSTALLER_ASSETS_DIR}/auth/kubeadmin-password"}
 # Default values
 OC_STABLE_LOGIN="false"
-CI_OPERATOR_HUB_PROJECT="ci-operator-hub-project"
+#CI_OPERATOR_HUB_PROJECT="ci-operator-hub-project"
 # Exported to current env
 export KUBECONFIG=${KUBECONFIG:-"${DEFAULT_INSTALLER_ASSETS_DIR}/auth/kubeconfig"}
 
@@ -49,11 +49,11 @@ fi
 # Setup the cluster for Operator tests
 
 ## Create a new namesapce which will be used for OperatorHub checks
-oc new-project $CI_OPERATOR_HUB_PROJECT
+#oc new-project $CI_OPERATOR_HUB_PROJECT
 ## Let developer user have access to the project
 oc adm policy add-role-to-user edit developer
 
-sh $SETUP_OPERATORS
+#sh $SETUP_OPERATORS
 # OperatorHub setup complete
 
 # Remove existing htpasswd file, if any
@@ -72,7 +72,31 @@ done
 
 # Workarounds - Note we should find better soulutions asap
 ## Missing wildfly in OpenShift Adding it manually to cluster Please remove once wildfly is again visible
-oc apply -n openshift -f https://raw.githubusercontent.com/openshift/library/master/arch/x86_64/community/wildfly/imagestreams/wildfly-centos7.json
+#oc apply -n openshift -f https://raw.githubusercontent.com/openshift/library/master/arch/x86_64/community/wildfly/imagestreams/wildfly-centos7.json
+oc import-image nodejs --from=registry.redhat.io/rhscl/nodejs-12-rhel7 --confirm -n openshift
+sleep 5
+oc annotate istag/nodejs:latest tags=builder -n openshift --overwrite
+oc import-image java:8 --namespace=openshift --from=registry.redhat.io/redhat-openjdk-18/openjdk18-openshift --confirm
+sleep 5
+oc annotate istag/java:8 --namespace=openshift tags=builder --overwrite
+oc apply -n openshift -f https://raw.githubusercontent.com/openshift/library/master/arch/s390x/official/ruby/imagestreams/ruby-rhel7-s390x.json
+sleep 5
+oc annotate istag/ruby:latest --namespace=openshift tags=builder --overwrite
+oc import-image wildfly --confirm \--from docker.io/clefos/wildfly-120-centos7:latest --insecure -n openshift
+sleep 5
+oc annotate istag/wildfly:latest --namespace=openshift tags=builder --overwrite
+oc apply -n openshift -f https://raw.githubusercontent.com/openshift/library/master/arch/s390x/official/nginx/imagestreams/nginx-rhel7-s390x.json
+sleep 5
+oc annotate istag/nginx:latest --namespace=openshift tags=builder --overwrite
+oc apply -n openshift -f https://raw.githubusercontent.com/openshift/library/master/community/dotnet/imagestreams/dotnet-centos7.json
+sleep 5
+oc annotate istag/dotnet:latest --namespace=openshift tags=builder --overwrite
+oc apply -n openshift -f https://raw.githubusercontent.com/openshift/library/master/arch/s390x/official/php/imagestreams/php-rhel7.json
+sleep 5
+oc annotate istag/php:latest --namespace=openshift tags=builder --overwrite
+oc apply -n openshift -f https://raw.githubusercontent.com/openshift/library/master/arch/s390x/official/python/imagestreams/python-rhel7.json
+sleep 5
+oc annotate istag/python:latest --namespace=openshift tags=builder --overwrite
 
 # Create secret in cluster, removing if it already exists
 oc get secret $HTPASSWD_SECRET -n openshift-config &> /dev/null
@@ -102,7 +126,9 @@ EOF
 # Login as developer and check for stable server
 for i in {1..40}; do
     # Try logging in as developer
-    oc login -u developer -p $USERPASS &> /dev/null
+    #oc login -u developer -p $USERPASS &> /dev/null
+    KUBEADMIN_PASSWORD=`cat $KUBEADMIN_PASSWORD_FILE`
+    oc login -u $KUBEADMIN_USER -p $KUBEADMIN_PASSWORD &> /dev/null
     if [ $? -eq 0 ]; then
         # If login succeeds, assume success
 	    OC_STABLE_LOGIN="true"
@@ -133,3 +159,4 @@ fi
 oc new-project myproject
 sleep 4
 oc version
+oc get secret pull-secret -n openshift-config -o yaml --export | oc create -f -
